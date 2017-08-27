dbb34a9f5b8a064f2b36f5206dad3ebf68de03d9
satvirgrewal <satvir.grewal03@gmail.com>
2017-08-27 13:25:55 -0500

    Check in

--- src/main/java/com/bankUserFront/controller/HomeController.java
+++ src/main/java/com/bankUserFront/controller/HomeController.java
@@ -1,7 +1,15 @@
 package com.bankUserFront.controller;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bankUserFront.domain.User;
 
 @Controller
 public class HomeController {
@@ -15,4 +23,32 @@ public class HomeController {
 	public String index(){
 		return "index";
 	}
+	
+	@RequestMapping(value="/signup", method=RequestMethod.GET)
+	public String signUp(Model model){
+		User user = new User();
+		model.addAttribute("user",user);
+		return "signup";
+		
+	}
+	
+	@RequestMapping(value="/signup", method=RequestMethod.POST)
+	public String signUpPost(@ModelAttribute("user") User user, Model model){
+		if(userService.checkUserExists(user.getUsername(), user.getEmail())){
+			if(userService.checkEmailExists(user.getEmail())){
+				model.addAttribute("emailExists", true);
+			}
+			if(userService.checkUsernameExists(user.getUsername())){
+				model.addAttribute("usernameExists", true);
+			}
+			return "signup";
+		}
+		else{
+			Set<UserRole> userRoles = new HashSet<>();
+			userRoles.add(new UserRole(user,roleDao.findByName("USER")));
+			userService.createUser(user,userRoles);
+		}
+		
+	}
+	
 }
